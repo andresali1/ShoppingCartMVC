3e05c038ded2ad8b205a5ce7f9fdc353060f40a0
Andrés Salinas <andresali1@hotmail.es>
2023-05-31 20:52:01 -0500

    Agregando funcionalidad de agregar y editar en Front End

--- DataLayer/DL_User.cs
+++ DataLayer/DL_User.cs
@@ -14,9 +14,10 @@ namespace DataLayer
 
             try
             {
-                using (SqlConnection oConection = new SqlConnection(Conection.connStr.Replace(@"\\", @"\")))
+                //using (SqlConnection oConection = new SqlConnection(Conection.connStr.Replace(@"\\", @"\")))
+                using (SqlConnection oConection = new SqlConnection(Conection.connStr))
                 {
-                    string query = "SELECT UserId, U_name, U_surname, Email, Pass, IsReset, Active FROM APP_USER";
+                    string query = "SELECT UserId, U_name, U_surname, Email, Active FROM APP_USER";
 
                     SqlCommand cmd = new SqlCommand(query, oConection);
                     cmd.CommandType = CommandType.Text;
@@ -30,18 +31,10 @@ namespace DataLayer
                             list.Add(new App_User()
                             {
                                 UserId = Convert.ToInt32(dr["UserId"])
-                                ,
-                                U_name = dr["U_name"].ToString()
-                                ,
-                                U_surname = dr["U_surname"].ToString()
-                                ,
-                                Email = dr["Email"].ToString()
-                                ,
-                                Pass = dr["Pass"].ToString()
-                                ,
-                                IsReset = Convert.ToBoolean(dr["IsReset"])
-                                ,
-                                Active = Convert.ToBoolean(dr["Active"])
+                                ,U_name = dr["U_name"].ToString()
+                                ,U_surname = dr["U_surname"].ToString()
+                                ,Email = dr["Email"].ToString()
+                                ,Active = Convert.ToBoolean(dr["Active"])
                             });
                         }
                     }
